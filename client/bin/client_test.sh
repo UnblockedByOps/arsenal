e0ce8a8a9a2fe9b9e4feb44f8800bb518197ed93
Aaron Bandt <abandt@rubiconproject.com>
2019-07-15 14:08:34 -0700

    INFRA-14868: fixing

--- client/bin/client_test.sh
+++ client/bin/client_test.sh
@@ -285,7 +285,7 @@ validate_command "${search_cmd} statuses search name=hibernating --fields all --
 # physical_locations
 #
 validate_command "${rw_cmd} physical_locations create --name TEST_LOCATION_1" 0
-validate_command "${rw_cmd} physical_locations create --name TEST_LOCATION_2" 0
+validate_command "${rw_cmd} physical_locations create --name TEST_LOCATION_2 -s CA" 0
 validate_command "${rw_cmd} physical_locations search name=TEST_LOCATION,admin_area=CA" 0
 
 #
