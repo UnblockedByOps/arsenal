b7870407ffed5f19a9f37c180da6cbc2a26bdd15
Jesse Escobedo <jescobedo@rubiconproject.com>
2019-08-20 11:11:42 -0700

    INFRA-15841: New version

--- server/setup.py
+++ server/setup.py
@@ -52,7 +52,7 @@ DEPENDENCY_LINKS = [
 ]
 
 setup(name='arsenalweb',
-      version='7.4',
+      version='7.5',
       description='Arsenal web api/ui',
       long_description=README + '\n\n' + CHANGELOG,
       classifiers=[
