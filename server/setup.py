d856b5339ff4fe0dd6b334e7270b927b98e5d169
Aaron Bandt <abandt@rubiconproject.com>
2020-03-11 09:24:48 -0700

    INFRA-18591: Failed to set the right perms on the delete routes for physical_*

--- server/setup.py
+++ server/setup.py
@@ -53,7 +53,7 @@ DEPENDENCY_LINKS = [
 ]
 
 setup(name='arsenalweb',
-      version='7.10',
+      version='7.11',
       description='Arsenal web api/ui',
       long_description=README + '\n\n' + CHANGELOG,
       classifiers=[
