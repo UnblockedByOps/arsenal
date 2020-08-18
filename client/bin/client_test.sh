c0bbb89ad3c4190b3849a1f743a55711bc8a492b
Aaron Bandt <abandt@rubiconproject.com>
2020-08-18 11:07:00 -0700

    INFRA-20850: Adding client tests

--- client/bin/client_test.sh
+++ client/bin/client_test.sh
@@ -192,6 +192,13 @@ validate_command "${rw_cmd} nodes search name=fopd-TEST8675.internal --tag test_
 validate_command "${search_cmd} nodes search name=fopd-TEST8675.internal --exact --fields tags" 0 "string" "value: my_string"
 validate_command "${rw_cmd} nodes search name=fopd-TEST8675.internal --del_tag test_string=my_string" 0
 #
+# hardware_profiles
+#
+validate_command "${rw_cmd} hardware_profiles search name=Unknown --rack-color '#000'" 0
+validate_command "${search_cmd} hardware_profiles search name=Unknown --exact --fields rack_color" 0 "string" "rack_color: '#000'"
+validate_command "${rw_cmd} hardware_profiles search name=Unknown --rack-u 6" 0
+validate_command "${search_cmd} hardware_profiles search name=Unknown --exact --fields rack_u" 0 "string" "rack_u: 6"
+#
 # Try to make changes as a read only user, be sure it doesn't let us.
 #
 # Create a node
