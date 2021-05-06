2b0f007f3c50edbb5239cff7f04b276c99f92ef6
Aaron Bandt <abandt@rubiconproject.com>
2021-05-06 09:02:52 -0700

    INFRA-23901: Standardize user/group/perm column names

--- server/arsenalweb/views/__init__.py
+++ server/arsenalweb/views/__init__.py
@@ -184,8 +184,8 @@ def db_groupfinder(userid):
 
     groups = None
     try:
-        user = DBSession.query(User).filter(User.user_name == userid).one()
-        groups = [group.group_name for group in user.groups]
+        user = DBSession.query(User).filter(User.name == userid).one()
+        groups = [group.name for group in user.groups]
     except NoResultFound:
         LOG.debug('No DB groups for: %s', userid)
     except Exception as ex:
@@ -200,7 +200,7 @@ def db_authenticate(login, password):
 
     try:
         q = DBSession.query(User)
-        q = q.filter(User.user_name == login)
+        q = q.filter(User.name == login)
         db_user = q.one()
     except Exception as e:
         LOG.debug("%s (%s)" % (Exception, e))
