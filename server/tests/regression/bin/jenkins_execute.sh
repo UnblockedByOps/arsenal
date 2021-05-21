3161d750321511ff80a3c33d7c96de4924aac813
Aaron Bandt <abandt@rubiconproject.com>
2021-05-21 13:55:01 -0700

    python3-server: testing branch

--- server/tests/regression/bin/jenkins_execute.sh
+++ server/tests/regression/bin/jenkins_execute.sh
@@ -7,26 +7,23 @@ REGRESSION_DIR="${WORKSPACE}/server/tests/regression"
 
 if [ ! -f "${WORKSPACE}/venv/bin/activate" ]; then
   echo -e "\nCreating virtualenv...\n"
-  /usr/bin/virtualenv -q ${WORKSPACE}/venv
+  /opt/rh/rh-python36/root/bin/virtualenv -q ${WORKSPACE}/venv
   source ${WORKSPACE}/venv/bin/activate
-  pip install -U pip==10.0.1 setuptools==44.0 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple PyNaCl==1.4.0 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple pyrsistent==0.16.0 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple attrs==19.3.0 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple cached-property==1.5.1 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple certifi==2020.6.20 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple cffi==1.14.0 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple chardet==3.0.4 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple cryptography==2.9.2 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple docker==4.2.2 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple paramiko==2.7.1 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple docker-compose==1.26.2 && \
+  pip install -U pip setuptools && \
+  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple alembic && \
+  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple paramiko && \
+  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple docker-compose && \
   pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple rp-retry==2.0 && \
-  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple ruamel.yaml==0.16.10 && \
+  pip install --trusted-host pypi -i http://pypi/nexus/repository/pypi-all/simple ruamel.yaml && \
   pip freeze | tee ${WORKSPACE}/venv/version.txt
 fi
 
 source ${WORKSPACE}/venv/bin/activate
+
+cd ${WORKSPACE}/server/
+echo 'use arsenal;' > ../tests/regression/docker/mysql_init/create.sql
+alembic -c development.ini upgrade head --sql >> ../tests/regression/docker/mysql_init/create.sql
+
 cd ${REGRESSION_DIR}/docker
 
 if [ "$ARSENAL_VERSION" == 'latest' ] ; then
