bc16ead1cbb49daee055b6842ce8ac836c68a3f4
Aaron Bandt <abandt@rubiconproject.com>
2021-06-15 15:34:55 -0700

    python3-server: testing branch

--- client/arsenalclient/authorization.py
+++ client/arsenalclient/authorization.py
@@ -187,6 +187,8 @@ class Authorization(object):
                 LOG.debug('Resp dir is: %s', dir(resp))
                 LOG.debug('Resp headers: %s', resp.headers)
                 LOG.debug('Resp cookies: %s', dir(resp.cookies))
+                for cooki in resp.cookies.items():
+                    LOG.debug('COOKI: %s', cooki)
                 LOG.debug('Cookies are: %s', self.session.cookies)
                 LOG.debug('Cookies are: %s', self.cookies)
                 try:
