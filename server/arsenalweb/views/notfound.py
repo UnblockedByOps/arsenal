c97b6992e3472f657769fac2c5cf80e5e051f3c5
Aaron Bandt <abandt@rubiconproject.com>
2021-07-26 10:01:27 -0700

    python3-server: testing branch

--- server/arsenalweb/views/notfound.py
+++ server/arsenalweb/views/notfound.py
@@ -1,9 +1,14 @@
 from pyramid.view import notfound_view_config
-
+from arsenalweb.views import site_layout
 
 @notfound_view_config(renderer='arsenalweb:templates/404.pt')
 def notfound_view(request):
     request.response.status = 404
     return {
+        'error': 'Page not found.',
+        'layout': site_layout('max'),
         'page_title_name': '404 Not Found',
+        'column_selectors': {},
+        'page_title_type': 'none',
     }
+
