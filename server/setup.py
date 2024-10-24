874c53f9a8f29d7fbaca6a83eac78110065f6e98
Aaron Bandt <aaronbandt@gmail.com>
2024-10-24 11:59:31 -0400

    mysql-update: Server version bump

--- server/setup.py
+++ server/setup.py
@@ -89,7 +89,7 @@ tests_require = [
 
 setup(
     name='arsenalweb',
-    version='12.6.1',
+    version='12.7.0',
     description='Arsenal web api/ui',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
